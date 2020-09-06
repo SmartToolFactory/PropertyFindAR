85d8e2b826daf6c09c47448973e4128ee0af7417
Smart Tool Factory <smarttoolfactory@icloud.com>
2020-09-06 21:59:25 +0300

    add data source interfaces

---
+++ libraries/data/src/main/java/com/smarttoolfactory/data/source/PropertyDataSource.kt
@@ -0,0 +1,44 @@
+package com.smarttoolfactory.data.source
+
+
+import com.smarttoolfactory.data.constant.ORDER_BY_NONE
+import com.smarttoolfactory.data.model.local.PropertyEntity
+import com.smarttoolfactory.data.model.remote.PropertyDTO
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+
+interface PropertyDataSource
+
+/*
+    Coroutines
+ */
+interface RemotePropertyDataSourceCoroutines : PropertyDataSource {
+    suspend fun getPropertyDTOs(orderBy: String = ORDER_BY_NONE): List<PropertyDTO>
+    suspend fun getPropertyDTOsWithPagination(
+        page: Int,
+        orderBy: String = ORDER_BY_NONE
+    ): List<PropertyDTO>
+}
+
+interface LocalPropertyDataSourceCoroutines : PropertyDataSource {
+    suspend fun getPropertyEntities(): List<PropertyEntity>
+    suspend fun saveEntities(properties: List<PropertyEntity>): List<Long>
+    suspend fun deletePropertyEntities()
+}
+
+/*
+    RxJava3
+ */
+interface RemotePropertyDataSourceRxJava3 : PropertyDataSource {
+    fun getPropertyDTOs(orderBy: String = ORDER_BY_NONE): Single<List<PropertyDTO>>
+    fun getPropertyDTOsWithPagination(
+        page: Int,
+        orderBy: String = ORDER_BY_NONE
+    ): Single<List<PropertyDTO>>
+}
+
+interface LocalPropertyDataSourceRxJava3 : PropertyDataSource {
+    fun getPropertyEntities(): Single<List<PropertyEntity>>
+    fun saveEntities(properties: List<PropertyEntity>): Completable
+    fun deletePropertyEntities(): Completable
+}
