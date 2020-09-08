23b5c2ff9c791ab2409986ab299c84dd02eba676
Smart Tool Factory <smarttoolfactory@icloud.com>
2020-09-08 17:21:55 +0300

    add Paged entity, mapper, dao, and Room migration

--- libraries/data/src/main/java/com/smarttoolfactory/data/db/PropertyDatabase.kt
+++ libraries/data/src/main/java/com/smarttoolfactory/data/db/PropertyDatabase.kt
@@ -6,11 +6,16 @@ import androidx.room.TypeConverters
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.smarttoolfactory.data.constant.DATABASE_VERSION
+import com.smarttoolfactory.data.model.local.PagedPropertyEntity
 import com.smarttoolfactory.data.model.local.PropertyEntity
 import com.smarttoolfactory.data.model.local.SortOrderEntity
 
 @Database(
-    entities = [PropertyEntity::class, SortOrderEntity::class],
+    entities = [
+        PropertyEntity::class,
+        PagedPropertyEntity::class,
+        SortOrderEntity::class
+    ],
     version = DATABASE_VERSION,
     exportSchema = true
 )
@@ -24,6 +29,8 @@ abstract class PropertyDatabase : RoomDatabase() {
     abstract fun propertySortDaoCoroutines(): SortOrderDaoCoroutines
 
     abstract fun propertySortDaoRxJava(): SortOrderDaoRxJava3
+
+    abstract fun pagedPropertyDao(): PagedPropertyDao
 }
 
 /**
@@ -37,6 +44,64 @@ val MIGRATION_1_2: Migration = object : Migration(1, 2) {
             "CREATE TABLE IF NOT EXISTS sort_filter " +
                 "(id INTEGER PRIMARY KEY  NOT NULL, " +
                 "order_by TEXT NOT NULL DEFAULT '')"
+        )
+    }
+}
+
+/**
+ * Add new Property table for paging, this could have been done with [PropertyEntity] but
+ * used this as another sample.
+ */
+val MIGRATION_2_3: Migration = object : Migration(2, 3) {
+
+    override fun migrate(database: SupportSQLiteDatabase) {
+
+        database.execSQL(
+            "CREATE TABLE IF NOT EXISTS `paged_property` (" +
+                    "`insert_order` INTEGER NOT NULL, " +
+                    "`id` INTEGER NOT NULL, " +
+                    "`update` INTEGER NOT NULL, " +
+                    "`category_id` INTEGER NOT NULL, " +
+                    "`title` TEXT NOT NULL, " +
+                    "`subject` TEXT NOT NULL, " +
+                    "`type` TEXT NOT NULL, " +
+                    "`type_id` INTEGER NOT NULL, " +
+                    "`thumbnail` TEXT, " +
+                    "`thumbnail_big` TEXT, " +
+                    "`image_count` INTEGER NOT NULL, " +
+                    "`price` TEXT NOT NULL, " +
+                    "`price_period` TEXT, " +
+                    "`price_period_raw` TEXT NOT NULL, " +
+                    "`price_label` TEXT, " +
+                    "`price_value` TEXT, " +
+                    "`price_value_raw` INTEGER NOT NULL, " +
+                    "`currency` TEXT NOT NULL, " +
+                    "`featured` INTEGER NOT NULL, " +
+                    "`location` TEXT NOT NULL, " +
+                    "`area` TEXT NOT NULL, " +
+                    "`poa` INTEGER NOT NULL, " +
+                    "`rera_permit` TEXT, " +
+                    "`bathrooms` TEXT NOT NULL, " +
+                    "`bedrooms` TEXT NOT NULL, " +
+                    "`date_insert` TEXT NOT NULL, " +
+                    "`date_update` TEXT NOT NULL, " +
+                    "`agent_name` TEXT NOT NULL, " +
+                    "`broker_name` TEXT NOT NULL, " +
+                    "`agent_license` TEXT, " +
+                    "`location_id` INTEGER NOT NULL, " +
+                    "`hide_location` INTEGER NOT NULL, " +
+                    "`broker` TEXT NOT NULL, " +
+                    "`amenities` TEXT NOT NULL, " +
+                    "`amenities_keys` TEXT NOT NULL, " +
+                    "`latitude` REAL NOT NULL, " +
+                    "`longitude` REAL NOT NULL, " +
+                    "`premium` INTEGER NOT NULL, " +
+                    "`livingrooms` TEXT NOT NULL, " +
+                    "`verified` INTEGER NOT NULL, " +
+                    "`gallery` TEXT, " +
+                    "`phone` TEXT NOT NULL, " +
+                    "`lead_email_receivers` TEXT NOT NULL, " +
+                    "`reference` TEXT NOT NULL, PRIMARY KEY(`insert_order`))"
         )
     }
 }
---
+++ libraries/data/src/main/java/com/smarttoolfactory/data/model/local/PagedPropertyEntity.kt
@@ -0,0 +1,111 @@
+package com.smarttoolfactory.data.model.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import com.smarttoolfactory.data.model.IEntity
+
+/**
+ * Properties for PAGING which are retrieved via REST is converted
+ * to [PagedPropertyEntity] to store in database for offline-first or online-first.
+ *
+ * * Implements [IEntity] marker interface for mapping this database items from REST DTOs or
+ * to UI items
+ */
+@Entity(tableName = "paged_property", primaryKeys = ["insert_order"])
+data class PagedPropertyEntity(
+
+    @ColumnInfo(name = "insert_order")
+    var insertOrder: Int = 0,
+
+    @ColumnInfo(name = "id")
+    val id: Int,
+    @ColumnInfo(name = "update")
+    val update: Int,
+    @ColumnInfo(name = "category_id")
+    val categoryId: Int,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "subject")
+    val subject: String,
+    @ColumnInfo(name = "type")
+    val type: String,
+    @ColumnInfo(name = "type_id")
+    val typeId: Int,
+    @ColumnInfo(name = "thumbnail")
+    val thumbnail: String?,
+    @ColumnInfo(name = "thumbnail_big")
+    val thumbnailBig: String?,
+    @ColumnInfo(name = "image_count")
+    val imageCount: Int,
+    @ColumnInfo(name = "price")
+    val price: String,
+    @ColumnInfo(name = "price_period")
+    val pricePeriod: String?,
+    @ColumnInfo(name = "price_period_raw")
+    val pricePeriodRaw: String,
+    @ColumnInfo(name = "price_label")
+    val priceLabel: String?,
+    @ColumnInfo(name = "price_value")
+    val priceValue: String?,
+    @ColumnInfo(name = "price_value_raw")
+    val priceValueRaw: Int,
+    @ColumnInfo(name = "currency")
+    val currency: String,
+    @ColumnInfo(name = "featured")
+    val featured: Boolean,
+    @ColumnInfo(name = "location")
+    val location: String,
+    @ColumnInfo(name = "area")
+    val area: String,
+    @ColumnInfo(name = "poa")
+    val poa: Boolean,
+    @ColumnInfo(name = "rera_permit")
+    val reraPermit: String?,
+    @ColumnInfo(name = "bathrooms")
+    val bathrooms: String,
+    @ColumnInfo(name = "bedrooms")
+    val bedrooms: String,
+    @ColumnInfo(name = "date_insert")
+    val dateInsert: String,
+    @ColumnInfo(name = "date_update")
+    val dateUpdate: String,
+    @ColumnInfo(name = "agent_name")
+    val agentName: String,
+    @ColumnInfo(name = "broker_name")
+    val brokerName: String,
+    @ColumnInfo(name = "agent_license")
+    val agentLicense: String?,
+    @ColumnInfo(name = "location_id")
+    val locationId: Int,
+    @ColumnInfo(name = "hide_location")
+    val hideLocation: Boolean,
+    @ColumnInfo(name = "broker")
+
+    val broker: BrokerEntity,
+    @ColumnInfo(name = "amenities")
+    val amenities: List<String>,
+    @ColumnInfo(name = "amenities_keys")
+    val amenitiesKeys: List<String>,
+
+    @ColumnInfo(name = "latitude")
+    val latitude: Double,
+    @ColumnInfo(name = "longitude")
+    val longitude: Double,
+    @ColumnInfo(name = "premium")
+    val premium: Boolean,
+    @ColumnInfo(name = "livingrooms")
+    val livingrooms: String,
+    @ColumnInfo(name = "verified")
+    val verified: Boolean,
+
+    @ColumnInfo(name = "gallery")
+    val gallery: List<String>?,
+    @ColumnInfo(name = "phone")
+    val phone: String,
+
+    @ColumnInfo(name = "lead_email_receivers")
+    val leadEmailReceivers: List<String>,
+
+    @ColumnInfo(name = "reference")
+    val reference: String
+) : IEntity
