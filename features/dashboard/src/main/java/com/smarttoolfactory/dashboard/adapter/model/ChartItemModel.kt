5e404e56a696d2f4eede94f326ca5d14f3429201
Smart Tool Factory <smarttoolfactory@icloud.com>
2020-09-30 01:57:10 +0300

    add ChartSectionViewBinder

--- features/dashboard/src/main/java/com/smarttoolfactory/dashboard/adapter/model/ChartItemModel.kt
+++
@@ -1,5 +0,0 @@
-package com.smarttoolfactory.dashboard.adapter.model
-
-import com.smarttoolfactory.domain.model.PropertyChartItem
-
-data class ChartItemModel(val items: List<PropertyChartItem>) : Model
---
+++ features/dashboard/src/main/java/com/smarttoolfactory/dashboard/adapter/viewholder/ChartSectionViewBinder.kt
@@ -0,0 +1,105 @@
+package com.smarttoolfactory.dashboard.adapter.viewholder
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayoutMediator
+import com.smarttoolfactory.core.ui.recyclerview.adapter.ItemBinder
+import com.smarttoolfactory.core.ui.recyclerview.adapter.MappableItemViewBinder
+import com.smarttoolfactory.core.ui.recyclerview.adapter.SingleViewBinderAdapter
+import com.smarttoolfactory.core.util.inflate
+import com.smarttoolfactory.dashboard.R
+import com.smarttoolfactory.dashboard.adapter.model.ChartSectionModel
+import com.smarttoolfactory.dashboard.databinding.ItemChartSectionBinding
+
+class ChartSectionViewBinder(
+    private val onChartItemClicked: ((Float) -> Unit)? = null,
+    private val pool: RecyclerView.RecycledViewPool? = null,
+) : MappableItemViewBinder<ChartSectionModel, ChartSectionViewHolder>(
+    ChartSectionModel::class.java
+) {
+
+    override fun createViewHolder(parent: ViewGroup): ChartSectionViewHolder {
+
+        return ChartSectionViewHolder(
+            parent.inflate(getItemLayoutResource()),
+            onChartItemClicked,
+            pool
+        )
+    }
+
+    override fun bindViewHolder(
+        model: ChartSectionModel,
+        viewHolder: ChartSectionViewHolder
+    ) {
+        viewHolder.bind(model)
+    }
+
+    override fun getItemLayoutResource() = R.layout.item_chart_section
+
+    override fun areItemsTheSame(
+        oldItem: ChartSectionModel,
+        newItem: ChartSectionModel
+    ): Boolean {
+        return oldItem.items == newItem.items
+    }
+
+    override fun areContentsTheSame(
+        oldItem: ChartSectionModel,
+        newItem: ChartSectionModel
+    ): Boolean {
+        return true
+    }
+
+    override fun onViewRecycled(viewHolder: ChartSectionViewHolder) {
+        viewHolder.onViewRecycled()
+        println("👻 Horizontal onViewRecycled: $viewHolder")
+    }
+
+    override fun onViewDetachedFromWindow(viewHolder: ChartSectionViewHolder) = Unit
+}
+
+@Suppress("UNCHECKED_CAST")
+class ChartSectionViewHolder(
+    internal val binding: ItemChartSectionBinding,
+    private val onChartItemClicked: ((Float) -> Unit)? = null,
+    private val pool: RecyclerView.RecycledViewPool?
+) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(model: ChartSectionModel) {
+
+        binding.tvType.text = model.chartTitle
+
+        // ViewPager2
+        val viewPager = binding.viewPager
+
+        // TabLayout
+        val tabLayout = binding.tabLayout
+
+        /*
+            Set Adapter for ViewPager inside this fragment using this Fragment,
+            more specifically childFragmentManager as param
+
+            🔥 Create FragmentStateAdapter with viewLifeCycleOwner
+            https://stackoverflow.com/questions/61779776/leak-canary-detects-memory-leaks-for-tablayout-with-viewpager2
+         */
+        viewPager.adapter =
+            SingleViewBinderAdapter(BarChartViewBinder(onChartItemClicked) as ItemBinder)
+                .apply {
+                    submitList(listOf(model, model))
+                }
+
+        // Bind tabs and viewpager
+        TabLayoutMediator(tabLayout, viewPager, tabConfigurationStrategy).attach()
+    }
+
+    internal fun onViewRecycled() = Unit
+
+    private val tabConfigurationStrategy =
+        TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+            when (position) {
+                0 -> tab.text = "Indices"
+                else -> tab.text = "Trends"
+            }
+        }
+}
